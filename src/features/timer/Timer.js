68511328f32a3d2a2b69c445ccb7ad45ce769f94
James <Dodds1990@gmail.com>
2021-08-20 17:30:53 -0400

    Change default timer to 5 minutes

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -10,7 +10,7 @@ import { RoundedButton } from '../../components/RoundedButton';
 
 import { Timing } from './Timing';
 
-const DEFAULT_TIME = 1;
+const DEFAULT_TIME = 5;
 export const Timer = ({ focusSubject, onTimerEnd, clearSubject }) => {
   useKeepAwake();
 
